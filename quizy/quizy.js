8507baf9e4858fd9f9d02e83bd046205a1b22aae
yohei tanaka <yohei0910@keio.jp>
2021-07-11 06:25:06 +0900

    （”正解・不正解のボックス追加”）

---
+++ quizy/quizy.js
@@ -0,0 +1,36 @@
+const choice1= document.getElementById('choice1');
+const choice2= document.getElementById('choice2');
+const choice3= document.getElementById('choice3');
+const answer_box= document.getElementById('answer-box');
+const non_answer_box= document.getElementById('non_answer-box');
+
+choice1.addEventListener('click' ,function(){
+
+    choice1.style.background='#FE5129';
+    choice1.style.color='white';
+    choice2.style.background='#2A7CFE';
+    choice2.style.color='white';
+
+    non_answer_box.style="display: block;";
+}
+)
+
+choice2.addEventListener('click' ,function(){
+    
+    choice2.style.background='#2A7CFE';
+    choice2.style.color='white';
+    
+    answer_box.style="display: block;";
+}
+)
+
+choice3.addEventListener('click' ,function(){
+
+    choice1.style.background='#FE5129';
+    choice1.style.color='white';
+    choice2.style.background='#2A7CFE';
+    choice2.style.color='white';
+
+    non_answer_box.style="display: block;";
+}
+)
