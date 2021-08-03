365362ca77493ae3b4fcf6af40c18c274b4870ec
yohei tanaka <yohei0910@keio.jp>
2021-08-03 18:42:27 +0900

    ヘッダーを追加

--- quizy/quizy.js
+++ quizy/quizy.js
@@ -1,7 +1,4 @@
 'use strict';
-const loop = document.getElementById('loop');
-
-
 //選択肢の配列
 let array = [['たかなわ','たかわ','こうわ'],//1問目の選択肢
             ['かめいど','かめど','かめと'],//2問目の選択肢
@@ -14,17 +11,6 @@ let array = [['たかなわ','たかわ','こうわ'],//1問目の選択肢
             ['ししぼね','しこね','ろっこつ'],//９問目の選択肢
             ['こぐれ','こばく','こしゃく'],//１０問目の選択肢
 ];
-
-let answer = ['たかなわ','かめいど','こうじまち','おなりもん','とどろき','しゃくじい','ぞうしき','おかちまち','ししぼね','こぐれ'];
-
-
-//選択肢をシャッフル
-for(let i=0; i<array.length ; i++ ){
-  let k  = Math.floor(Math.random() * 3);//0,1,2の中からランダムな数字を取得
-  let l  = Math.floor(Math.random() * 3);//0,1,2の中からランダムな数字を取得
-  [array[i][k], array[i][l]] = [array[i][l], array[i][k]];//シャッフル
-};
- 
 
 //画像の配列
 let pic =['https://d1khcm40x1j0f.cloudfront.net/quiz/34d20397a2a506fe2c1ee636dc011a07.png',//画像１
@@ -44,46 +30,50 @@ let pic =['https://d1khcm40x1j0f.cloudfront.net/quiz/34d20397a2a506fe2c1ee636dc0
 let main = ''  ;
 for(let i=0; i<array.length; i++  ){
   main +=
+   //問題内容と選択肢を表示
      '<div>'
-    +'<h1 class="title"><span class="under">' + [i+1] +'.この地名は</span>なんて読む？</h1>'//問題１を１０個表示
-    +'<div class="pic" style="text-align: center">'
-    +'<img src="'+pic[i] +'" alt="問題画像" width="100%"></div>' //各問ごとに画像を表示
-    +'<ul id=choice_order>'
-    +'<li class="choice" id ="choice'+[i]+'_1" onclick="click_choices('+[i]+',0,0)" ><b>' + array[i][0] +'</b></li>'//選択肢の表示
-    +'<li class="choice" id ="choice'+[i]+'_2" onclick="click_choices('+[i]+',1,0)"><b>' + array[i][1]+'</b></li>'//選択肢の表示
-    +'<li class="choice" id ="choice'+[i]+'_3" onclick="click_choices('+[i]+',2,0)"><b>' + array[i][2]+'</b></li>'//選択肢の表示
-    +'</ul>'
-    +'</div> </div> '
+    +'<h2 class="title"><span class="under">' + [i+1] +'.この地名は</span>なんて読む？</h2>'//問題１を１０個表示
+    + '<div class="pic" style="text-align: center">'
+    +   '<img src="'+pic[i] +'" alt="問題画像" width="100%"></div>' //各問ごとに画像を表示
+    +     '<ul id="worksFigure_'+i+'" style="display: flex;flex-direction: column">'
+    +        '<li class="choice" id ="choice'+i+'_1" onclick="click_choices('+i+',0,0)" ><b>' + array[i][0] +'</b></li>'//選択肢の表示
+    +        '<li class="choice" id ="choice'+i+'_2" onclick="click_choices('+i+',1,0)"><b>' + array[i][1]+'</b></li>'//選択肢の表示
+    +        '<li class="choice" id ="choice'+i+'_3" onclick="click_choices('+i+',2,0)"><b>' + array[i][2]+'</b></li>'//選択肢の表示
+    +     '</ul>'
+    +  '</div> '
+    +'</div> '
     //正解ボックス(初めは見えていない)
-    +'<div class="answer_box" id="answer_box'+[i]+'">'
-    +'<span class="answer">正解!</span>'
-    +'<p>正解は「' + answer[i] + '」です！</p></div>'
+    +'<div class="answer_box" id="answer_box'+i+'">'
+    + '<span class="answer">正解!</span>'
+    + '<p>正解は「' + array[i][0] + '」です！</p>'
+    +'</div>'
     //不正解ボックス(初めは見えていない)
-    +'<div class="non_answer_box" id= "non_answer_box'+[i]+'">'
-    +'<span class="non_answer">不正解！</span>'
-    +'<p>正解は「' + answer[i] + '」です！</p>'
+    +'<div class="non_answer_box" id= "non_answer_box'+i+'">'
+    + '<span class="non_answer">不正解！</span>'
+    + '<p>正解は「' + array[i][0] + '」です！</p>'
     +'</div>'
   //mainをhtml上に表示
   loop.innerHTML  = main;
-
-
-}
-
-
-
-
-
+  // 要素の取得
+  let list_items = document.querySelectorAll("#worksFigure_"+i+" li");
+  // ランダム関数
+  function rand() {
+    let choice_order = Math.floor(Math.random() * 3 );
+    // console.log(choice_order);
+    return choice_order;
+  }
+  // <li>のorderだけ修正して並び直す
+  Array.from(list_items).forEach( elm =>{ elm.style.order=rand()});
+};
 
 //1問目から１０問目までの正誤判定
 function click_choices(quizNum,optionNum,correctNum){
-
   
   let choice1 = document.getElementById("choice" + quizNum + "_1");
   let choice2 = document.getElementById("choice" + quizNum + "_2");
   let choice3 = document.getElementById("choice" + quizNum + "_3");
   let answer_box  = document.getElementById("answer_box" + quizNum );
   let non_answer_box  = document.getElementById("non_answer_box" + quizNum );
-
   if(optionNum === correctNum){
     //正解を選択した際の処理
     choice1.style.background='#2A7CFE';
