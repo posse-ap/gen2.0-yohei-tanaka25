01765d441e590120dc88a498eaf5c8551f7d7928
yohei tanaka <yohei0910@keio.jp>
2021-10-15 15:10:32 +0900

    モーダルのカレンダーとロードを追加

--- webapp.js
+++ webapp.js
@@ -20,10 +20,43 @@
 //   }
 //   popupImage();
 
+
 var showBtn = document.getElementById('open_modal');
+var closeBtn = document.getElementById('close_modal');
 var popup = document.getElementById('modal_area');
+var overlay = document.getElementById('overlay');
 
 showBtn.addEventListener('click', function(){
     popup.classList.add('show');
-    console.log('aa');
-})
+    overlay.classList.add('overlay');
+
+});
+
+closeBtn.addEventListener('click', function(){
+    popup.classList.remove('show');
+    overlay.classList.remove('overlay');
+});
+
+// カレンダー
+var sample = document.getElementById('sample');
+var fp = flatpickr(sample, {
+    enableTime: true,
+    dateFormat: "Y-m-d",// フォーマットの変更
+});
+
+// ロード
+var modal_main = document.getElementById('modal_main');
+var load = document.getElementById('load');
+var tweet = document.getElementById('tweet');
+// var awesome = document.getElementById('awesome');
+
+tweet.addEventListener('click', function(){
+    modal_main.classList.add('in_show');
+    load.classList.add('show_load');
+    tweet.classList.add('in_show');
+    // awesome.classList.add('show_awesome');
+
+});
+
+
+
